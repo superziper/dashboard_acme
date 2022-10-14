0b961f704c294e9b4d3da184b029756248fbaabf
zulvanavivi <67460472+superziper@users.noreply.github.com>
2022-10-14 23:19:33 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,8 +3,10 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO ACME SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: white;'>a portfolio by Zulvan Avivi</h4>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; color: white;'>select menu through left sidebar</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: white;'>first loading might take some time (90 MB sales data need to be downloaded)</h4>", unsafe_allow_html=True)
 
 
 
