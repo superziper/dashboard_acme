9163b14317bc3cd1f722d2fd17460257a8e9b467
zulvanavivi <67460472+superziper@users.noreply.github.com>
2022-10-14 22:50:43 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout="wide")
+
+st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO ACME SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: white;'>select menu through left sidebar</h4>", unsafe_allow_html=True)
+
+
+
+
