d1c3ed2c9dae817321f3421a6ae220a61c7160c9
zulvanavivi <67460472+superziper@users.noreply.github.com>
2022-10-16 22:00:23 +0700

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -4,9 +4,24 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
-st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO ACME SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; color: white;'>select menu through left sidebar</h4>", unsafe_allow_html=True)
 
+import streamlit as st
 
+footer="""<style>
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed by Zulvan Avivi<a style='display: block; text-align: center;'</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
 
