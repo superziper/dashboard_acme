aa255641072dbc18559d5150fc9b8fd3f1c32cc7
zulvanavivi <67460472+superziper@users.noreply.github.com>
2022-10-16 19:48:56 +0700

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
-st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center; color: white;'>a portfolio by Zulvan Avivi</h4>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO ACME SALES DATA DASHBOARD</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; color: white;'>select menu through left sidebar</h4>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center; color: white;'>first loading might take some time (90 MB sales data need to be downloaded)</h4>", unsafe_allow_html=True)
 
 
 
